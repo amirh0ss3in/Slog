279dcdeb93d3ef017350d7b9357407d234ba96d6
amirh0ss3in <59564278+amirh0ss3in@users.noreply.github.com>
2021-12-19 13:50:12 +0330

    Create slog.py

---
+++ slog.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt 
+
+def symp(data,x_scale,y_scale):
+    plt.plot(data[0],data[1])
+    plt.xscale(x_scale)
+    plt.yscale(y_scale)
+    plt.show()
+
+def main():
+    # symp(data,'symlog''symlog')
+    pass
+
+if __name__ == '__main__':
+    main()
